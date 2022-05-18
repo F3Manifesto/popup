d7e48fd54c52b9a20b27a4fbd57523ff3c10ae75
James Lin <wonyy1988@outlook.com>
2022-05-18 09:06:53 -0400

    fix: carousel issue

--- components/common/ProductTopBanner/index.js
+++ components/common/ProductTopBanner/index.js
@@ -1,27 +1,26 @@
-import { FC, useState } from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-import { TextSlider } from "..";
+import { useState, memo } from "react";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+import { Carousel } from "react-responsive-carousel";
 import HomeTitle from "@components/ui/HomeTitle";
 import { HeroBar } from "@components/ui";
 import Container from "@components/ui/Container";
 import shuffle from "@lib/shuffle";
 import styles from "./ProductTopBanner.module.scss";
 
-interface Props {
-  showSlider?: boolean;
-  showFilterbar?: boolean;
-  isHomePage?: boolean;
-  isExplorePage?: boolean;
-  filter?: string;
-  setFilter(value: string): void;
-  setSortBy(sort: string): void;
-}
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const ProductTopBanner: FC<Props> = ({
+// interface Props {
+//   showSlider?: boolean;
+//   showFilterbar?: boolean;
+//   isHomePage?: boolean;
+//   isExplorePage?: boolean;
+//   filter?: string;
+//   setFilter(value: string): void;
+//   setSortBy(sort: string): void;
+// }
+
+const ProductTopBanner = ({
   showSlider = false,
   showFilterbar = false,
   isHomePage = false,
@@ -30,25 +29,25 @@ const ProductTopBanner: FC<Props> = ({
   setFilter = () => {},
   setSortBy = () => {},
 }) => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+  // const responsive = {
+  //   superLargeDesktop: {
+  //     // the naming can be any, depends on you.
+  //     breakpoint: { max: 4000, min: 3000 },
+  //     items: 1,
+  //   },
+  //   desktop: {
+  //     breakpoint: { max: 3000, min: 1024 },
+  //     items: 1,
+  //   },
+  //   tablet: {
+  //     breakpoint: { max: 1024, min: 464 },
+  //     items: 1,
+  //   },
+  //   mobile: {
+  //     breakpoint: { max: 464, min: 0 },
+  //     items: 1,
+  //   },
+  // };
 
   const banners = [
     "/images/banners/bridgeshot.jpg",
@@ -75,30 +74,15 @@ const ProductTopBanner: FC<Props> = ({
         )}
         {isHomePage && (
           <div className={styles.carouselWrapper}>
-            {/* <Carousel
+            <Carousel
               infiniteLoop={true}
               showThumbs={false}
               showIndicators={false}
               showStatus={false}
-              showArrows={true}
+              showArrows={false}
               autoPlay={true}
               stopOnHover={false}
               interval={10000}
-            > */}
-            <Carousel
-              ssr
-              autoPlay
-              autoPlaySpeed={5000}
-              arrows={false}
-              infinite={true}
-              renderButtonGroupOutside={false}
-              renderDotsOutside={false}
-              slidesToSlide={1}
-              deviceType={"desktop"}
-              itemClass={styles.bannerItem}
-              responsive={responsive}
-              draggable={false}
-              swipeable={false}
             >
               {randomOrder.map((item) => {
                 return (
@@ -130,9 +114,8 @@ const ProductTopBanner: FC<Props> = ({
           />
         )}
       </Container>
-      {showSlider && <TextSlider black={false} />}
     </div>
   );
 };
 
-export default ProductTopBanner;
+export default memo(ProductTopBanner);
